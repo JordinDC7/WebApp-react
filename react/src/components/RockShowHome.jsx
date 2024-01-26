2431748041c3052e9a9474357d010ff9305545ee
JordinDC7 <jordincamp23@outlook.com>
2024-01-25 21:43:24 -0800

    adding files

---
+++ react/src/components/RockShowHome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function RockShowHome() {
+  return (
+    <div className="container">
+      <h1>RockShowHome</h1>
+    </div>
+  );
+}
+
+export default RockShowHome;
