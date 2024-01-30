3fd6b968353bd310f9a223652919b9dc9e54bb5c
JordinDC7 <jordincamp23@outlook.com>
2024-01-30 01:46:04 -0800

    Finally have a production build done

--- src/service/shop.js
+++ src/service/shop.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const rockShowService = {
-    endpoint: "https://oracleillusions.azurewebsites.net"
+    endpoint: "https://oracleillusions.azurewebsites.net/api"
 }
 
 //const prodString =  "https://oracleillusions.azurewebsites.net/api";
